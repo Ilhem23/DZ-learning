036e87b5dfe170735640314ea3ff48a1289ed5a9
Aissaoui <ei_aissaoui@esi.dz>
2019-02-28 17:59:05 +0100

    initial commit

---
+++ mod/scorm/report/download_scores/version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Download scores report for SCORM
+ * @package    scormreport
+ * @subpackage download_scores
+ * @author     Christian Perfect for Newcastle University
+ * @license    http://www.apache.org/licenses/LICENSE-2.0.html Apache License, version 2.0
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2014102100;
+$plugin->component = 'scormreport_download_scores';
